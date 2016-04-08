b0c63ba4d8ed4a3fe52e7cadda706753dc5f7cec
Riva Kepych <rivakepych@dhcpr184.fit.vutbr.cz>
2016-04-08 14:51:52 +0200

    fixed typo in file path to data

--- files.py
+++ files.py
@@ -3,7 +3,7 @@
 with open("files_source.txt", "w") as file:
 	for i in range(1, 5851):
 		num_str = str(i).zfill(5)
-		path = "/mnt/matylda2/data/FISHER/fe_03_pl_tran/data/trans/" + num_str[:3] + "/fe_03_" + num_str + ".txt"
+		path = "/mnt/matylda2/data/FISHER/fe_03_p1_tran/data/trans/" + num_str[:3] + "/fe_03_" + num_str + ".txt"
 		file.write(path + "\n")
 
 with open("files_counts.txt", "w") as file:
