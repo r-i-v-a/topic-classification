eb417ce28836ba4efa1840944ca1283ad4915efc
Riva Kepych <rivakepych@dhcpr184.fit.vutbr.cz>
2016-04-08 15:42:41 +0200

    eliminate terms containing only . or _

--- counts.sh
+++ counts.sh
@@ -7,5 +7,5 @@ while read d; do
 done < files_subdirectories.txt
 
 paste files_source.txt files_counts.txt | while read fs fc; do
-	tr -c "[a-z'_.]" "\n" < $fs | sed '/^$/d' | sort | uniq -c > $fc
+	tr -c "[a-z'_.]" "\n" < $fs | sed '/^[._]*$/d' | sort | uniq -c > $fc
 done
