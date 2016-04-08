14fc5c045bf8aab1c6a9d36f9a88d4bdf2887368
Riva Kepych <rivakepych@dhcpr184.fit.vutbr.cz>
2016-04-08 17:35:18 +0200

    updated main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# make lists of file paths
+echo 'making lists of file paths'
 ./files.py
 
-# make subdirectories for counts
+echo 'making subdirectories for counts'
 rm -r counts/*
 while read d; do
 	mkdir $d
 done < files_subdirectories.txt
 
-# get term counts from transcript files
+echo 'getting term counts from each document'
 paste files_source.txt files_counts.txt | while read fs fc; do
 	tr -c "[a-z'_.]" "\n" < $fs | sed '/^[._]*$/d' | sort | uniq -c > $fc
 done
