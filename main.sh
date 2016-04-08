d83d8c28c7d95df6df4f2aec2861423557ac40fa
Riva Kepych <rivakepych@dhcpr184.fit.vutbr.cz>
2016-04-08 18:27:40 +0200

    added preprocessing step, now called vectors.py, to main script + clearing all output folders at the beginning

--- main.sh
+++ main.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo 'clearing output folders'
+rm -r counts/*
+rm -r vectors_mi/*
+rm -r vectors_x2/*
+
 echo 'making lists of file paths'
 ./files.py
 
 echo 'making subdirectories for counts'
-rm -r counts/*
 while read d; do
 	mkdir $d
 done < files_subdirectories.txt
@@ -13,3 +17,6 @@ echo 'getting term counts from each document'
 paste files_source.txt files_counts.txt | while read fs fc; do
 	tr -c "[a-z'_.]" "\n" < $fs | sed '/^[._]*$/d' | sort | uniq -c > $fc
 done
+
+echo 'making document vectors'
+./vectors.py
